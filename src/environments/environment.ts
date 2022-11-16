e87d6b7a076c6221d4de53abc5f28f4a95a2632a
ShubhamSBhat <shubham.s.bhat@gmail.com>
2022-11-16 10:58:40 +0530

    front end

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,16 @@
 
 export const environment = {
   production: false,
-  firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
-  }
+  firebase: {
+    apiKey: "AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y",
+    authDomain: "memo.firebaseapp.com",
+    databaseURL: "https://memo.firebaseio.com",
+    projectId: "memo",
+    storageBucket: "memo.appspot.com",
+    messagingSenderId: "122050526979",
+    appId: "1:966683128720:web:94ce9d840500d16c7ef915",
+  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
